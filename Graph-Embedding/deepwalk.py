fda201f5e2f7e7eee29a334cc282a55378a9885c
Zixi Liu <46979228+zixi-liu@users.noreply.github.com>
2022-03-06 00:48:44 -0800

    Add files via upload

---
+++ Graph-Embedding/deepwalk.py
@@ -0,0 +1,107 @@
+
+from gensim.models import Word2Vec
+import pandas as pd
+import random
+import itertools
+from joblib import Parallel, delayed
+
+
+
+# Utils
+def partition_num(num, workers):
+    if num % workers == 0:
+        return [num//workers]*workers
+    else:
+        return [num//workers]*workers + [num % workers]
+
+# Random Walker
+class RandomWalker:
+    def __init__(self, G, p=1, q=1):
+        """
+        :param G:
+        :param p: Return parameter,controls the likelihood of immediately revisiting a node in the walk.
+        :param q: In-out parameter,allows the search to differentiate between “inward” and “outward” nodes
+        """
+        self.G = G
+        self.p = p
+        self.q = q
+
+    def deepwalk_walk(self, walk_length, start_node):
+
+        walk = [start_node]
+
+        while len(walk) < walk_length:
+            cur = walk[-1]
+            cur_nbrs = list(self.G.neighbors(cur))
+            if len(cur_nbrs) > 0:
+                walk.append(random.choice(cur_nbrs))
+            else:
+                break
+        return walk
+
+    def simulate_walks(self, num_walks, walk_length, workers=1, verbose=0):
+
+        G = self.G
+
+        nodes = list(G.nodes())
+
+        results = Parallel(n_jobs=workers, verbose=verbose, )(
+            delayed(self._simulate_walks)(nodes, num, walk_length) for num in
+            partition_num(num_walks, workers))
+
+        walks = list(itertools.chain(*results))
+
+        return walks
+
+    def _simulate_walks(self, nodes, num_walks, walk_length, ):
+        walks = []
+        for _ in range(num_walks):
+            random.shuffle(nodes)
+            for v in nodes:
+                if self.p == 1 and self.q == 1:
+                    walks.append(self.deepwalk_walk(
+                        walk_length=walk_length, start_node=v))
+        return walks
+
+
+# Deep Walk
+class DeepWalk:
+    def __init__(self, graph, walk_length, num_walks, workers=1):
+
+        self.graph = graph
+        self.w2v_model = None
+        self._embeddings = {}
+
+        self.walker = RandomWalker(
+            graph, p=1, q=1, )
+        self.sentences = self.walker.simulate_walks(
+            num_walks=num_walks, walk_length=walk_length, workers=workers, verbose=1)
+
+    def train(self, embed_size=128, window_size=5, workers=3, iter=5, **kwargs):
+
+        kwargs["sentences"] = self.sentences
+        kwargs["min_count"] = kwargs.get("min_count", 0)
+        kwargs["vector_size"] = embed_size
+        kwargs["sg"] = 1  # skip gram
+        kwargs["hs"] = 1  # deepwalk use Hierarchical Softmax
+        kwargs["workers"] = workers
+        kwargs["window"] = window_size
+        kwargs["epochs"] = iter
+
+        print("Learning embedding vectors...")
+        model = Word2Vec(**kwargs)
+        print("Learning embedding vectors done!")
+
+        self.w2v_model = model
+        return model
+
+    def get_embeddings(self,):
+        if self.w2v_model is None:
+            print("model not train")
+            return {}
+
+        self._embeddings = {}
+        for word in self.graph.nodes():
+            self._embeddings[word] = self.w2v_model.wv[word]
+
+        return self._embeddings
